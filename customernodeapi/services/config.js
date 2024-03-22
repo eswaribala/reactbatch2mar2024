8d720ccb5d2c03d89943d0e8d64f9cdc6c57f6f2
Parameswari Bala <parameswaribala@gmail.com>
2024-03-22 18:01:34 +0530

    updated

---
+++ customernodeapi/services/config.js
@@ -0,0 +1,5 @@
+exports.url='127.0.0.1'
+exports.mongodb='chitdb'
+exports.mongoport=27017
+
+
---
+++ customernodeapi/services/customerservice.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+var CustomerModel= require('../schemas/customerschema').CustomerModel;
+
+mongoose.connect('mongodb://localhost:27017/chitdb');
+
+
+//mongoose.connect(config.url, config.mongodb, config.mongoport);
+module.exports.Add=function(obj)
+{
+    var db = mongoose.connection;
+    db.once('open', function() {
+    });
+
+    var obj = new CustomerModel(
+        {
+            customerId:0,
+            firstName:obj.firstName,
+            lastName:obj.lastName,
+            email:obj.email,
+            password:obj.password,
+            phone:obj.mobileNo
+
+        });
+    obj.save(function(err,result){
+        if(!err)
+            console.log(result);
+
+    });
+
+}
+
+
+
+
+
+
+
