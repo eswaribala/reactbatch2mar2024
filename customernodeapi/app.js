794589621b5dee1530030a61586cdaa13e576606
Parameswari Bala <parameswaribala@gmail.com>
2024-03-22 16:15:30 +0530

    updated

---
+++ customernodeapi/app.js
@@ -0,0 +1,23 @@
+var customerRouter=require('./routes/customerroute')
+const express = require("express");
+const {json, urlencoded} = require("body-parser");
+const cors=require('cors')
+var app = express();
+
+app.use(json());
+app.use(urlencoded({extended:true}));
+app.use(cors());
+
+//app.use('/users', usersRouter);
+app.use('/api', customerRouter)
+
+
+
+
+
+app.set('port',4000);
+app.listen(app.get('port'), function(){
+    console.log('The server is running, ' +
+        ' please open your browser at http://localhost:%s',
+        app.get('port'));
+});
--- customernodeapi/routes/customerroute.js
+++ customernodeapi/routes/customerroute.js
@@ -1,9 +1,30 @@
 var express=require('express')
 var router=express.Router();
 
-router.post("/customers",function(req
-                                  ,res){
-
+router.post('/customers', function(req, res, next) {
+    console.log("Testing...")
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    res.setHeader('Access-Control-Allow-Credentials', true);
     console.log(req.body);
+    //console.log(request);
+    // refAddState.Add(request.query.ID,request.query.Name);
 
-})
+    res.end("Object received successfully");
+});
+router.get('/customers', function(req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    //console.log(req.body);
+    //console.log(request);
+    // refAddState.Add(request.query.ID,request.query.Name);
+
+    res.end("Ready to Rock!!!!");
+});
+
+
+module.exports = router;
