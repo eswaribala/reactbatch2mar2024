4017486af362293659e6a225adeb73121bf295d2
Parameswari Bala <parameswaribala@gmail.com>
2024-03-28 15:59:07 +0530

    updated

--- PolicyAPI/PolicyAPI/Program.cs
+++ PolicyAPI/PolicyAPI/Program.cs
@@ -1,9 +1,22 @@
+using Microsoft.Data.SqlClient;
+using PolicyAPI.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+Dictionary<string,Object> result = new VaultConfiguration(configuration)
+    .GetSecrets().Result;
+SqlConnectionStringBuilder providerCs 
+    = new SqlConnectionStringBuilder();
+providerCs.UserID = result["username"].ToString();
+providerCs.Password = result["password"].ToString();
+
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
