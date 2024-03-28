b6b625cf389532ab1079454f47bf20cbffd98ad8
Parameswari Bala <parameswaribala@gmail.com>
2024-03-28 14:23:35 +0530

    updated

---
+++ PolicyAPI/PolicyAPI/Models/FullName.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PolicyAPI.Models
+{
+    [Owned]
+    public class FullName
+    {
+        [Required]
+        [Column("First_Name" , TypeName = "varchar(100)")]
+        [RegularExpression("^[a-zA-Z]{5,25}$", 
+            ErrorMessage = "First Name Should be in alphabets within the range of 5,25")]
+        public string FirstName {  get; set; } = string.Empty;
+        [Required]
+        [Column("Last_Name", TypeName = "varchar(100)")]
+        [RegularExpression("^[a-zA-Z]{5,25}$",
+            ErrorMessage = "Last Name Should be in alphabets within the range of 5,25")]
+        public string LastName { get; set; }=string.Empty;
+        [Column("Middle_Name", TypeName = "varchar(50)")]
+
+        public string MiddleName {  get; set; } = string.Empty; 
+    }
+}
