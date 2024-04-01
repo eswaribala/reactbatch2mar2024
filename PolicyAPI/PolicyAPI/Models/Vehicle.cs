aa407ddc467074cb5f3c472cbf26e9267cf92a7c
Parameswari Bala <parameswaribala@gmail.com>
2024-04-01 18:21:33 +0530

    updated

--- PolicyAPI/PolicyAPI/Models/Vehicle.cs
+++ PolicyAPI/PolicyAPI/Models/Vehicle.cs
@@ -33,5 +33,6 @@ namespace PolicyAPI.Models
         [Required]
         public string Color {  get; set; }
 
+
     }
 }
---
+++ PolicyAPI/PolicyAPI/Queries/RootQuery.cs
@@ -0,0 +1,35 @@
+﻿
+
+using GraphQL;
+using GraphQL.Types;
+using PolicyAPI.Repositories;
+
+namespace PolicyAPI.Queries
+{
+    public class RootQuery:ObjectGraphType
+    {
+
+        public RootQuery(IVehicleRepo vehicleRepo) {
+
+            //all vehicles
+            Field<ListGraphType<VehicleGQLType>>(
+            "vehicles",
+                    resolve: context => vehicleRepo.GetAllVehicles());
+
+
+
+            //get vehicle by id
+            Field<VehicleGQLType>(
+               "vehicle",
+               arguments: new QueryArguments(new
+               QueryArgument<StringGraphType>
+               { Name = "registrationNo" }),
+               resolve: context => vehicleRepo
+               .GetVehicleById(context.GetArgument<string>("registrationNo"))
+
+               );
+
+        }
+
+    }
+}
