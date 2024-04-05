1cdbc4d3a06b067f8a5b74940977d0386f4d8068
Parameswari Bala <parameswaribala@gmail.com>
2024-04-05 18:05:03 +0530

    updated

--- PolicyAPI/PolicyAPI/Controllers/AuthenticationController.cs
+++ PolicyAPI/PolicyAPI/Controllers/AuthenticationController.cs
@@ -2,7 +2,11 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using PolicyAPI.Auth;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace PolicyAPI.Controllers
 {
@@ -15,18 +19,18 @@ namespace PolicyAPI.Controllers
     public class AuthenticationController : ControllerBase
     {
         private readonly UserManager<IdentityUser> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;    
+        private readonly RoleManager<IdentityRole> _roleManager;
         private IConfiguration _configuration;
-        
+
         public AuthenticationController(UserManager<IdentityUser> userManager,
-            RoleManager<IdentityRole> roleManager, 
-           
+            RoleManager<IdentityRole> roleManager,
+
             IConfiguration configuration
             )
         {
             this._userManager = userManager;
             this._roleManager = roleManager;
-          //  this._logger = logger;
+            //  this._logger = logger;
             this._configuration = configuration;
 
         }
@@ -96,7 +100,69 @@ namespace PolicyAPI.Controllers
             return Ok(new Response { Status = "Success", StatusMessage = "User created successfully!" });
         }
 
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+
+                var token = GetToken(authClaims);
+                /*
+                 * GET inventoryindex-2023-05/_search
+                        {
+                          "query": {
+                        "match": {
+                              "message": "Token"
+                            }
+                          }
+                        }
+                 */
+
+               // _logger.LogInformation($"Token Generated as {token.Payload},{token.EncodedPayload} for the user {model.UserName}");
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
 
 
+        }
+        private JwtSecurityToken GetToken(List<Claim> authClaims)
+        {
+            // Add services to the container.
+            //Dictionary<string, Object> data = new VaultConfiguration(_configuration)
+            //     .GetSecret().Result;
+
+            var data = _configuration["Secret"];
+
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.
+                GetBytes(data));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddMinutes(5),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+
+            return token;
+        }
     }
 }
