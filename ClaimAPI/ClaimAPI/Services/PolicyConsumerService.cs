e9207bcfba920c5a34a8fbbac2baec209aaadb31
Parameswari Bala <parameswaribala@gmail.com>
2024-04-08 18:16:40 +0530

    updated

--- ClaimAPI/ClaimAPI/Services/PolicyConsumerService.cs
+++ ClaimAPI/ClaimAPI/Services/PolicyConsumerService.cs
@@ -1,11 +1,42 @@
 ﻿
+using ClaimAPI.Repositories;
+using Confluent.Kafka;
+
 namespace ClaimAPI.Services
 {
     public class PolicyConsumerService : BackgroundService
     {
+        private IPolicyRepo _policyRepo;
+        private IConfiguration _configuration;
+
+        public PolicyConsumerService(IPoicyRepo policyRepo,
+            IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _policyRepo = policyRepo;
+
+        }
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            throw new NotImplementedException();
+            var groupId = _configuration["GroupId"];
+            var bootStrapServer = _configuration["BootstrapServer"];
+            var topicName = _configuration["TopicName"];
+
         }
+
+        public Task<string> ConsumePolicyData(string TopicName, 
+            string Group_Id, string BootstarpServer)
+        {
+
+            var consumerConfig = new ConsumerConfig
+            {
+                BootstrapServers=BootstarpServer,
+                GroupId=Group_Id,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+        }
+
+
     }
 }
