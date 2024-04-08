79e13509c701d79f8069f73a7de051bdda411e7d
Parameswari Bala <parameswaribala@gmail.com>
2024-04-08 18:03:38 +0530

    updated

---
+++ ClaimAPI/ClaimAPI/Repositories/PolicyRepo.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.Auth.AccessControlPolicy;
+using MongoDB.Driver;
+
+namespace ClaimAPI.Repositories
+{
+    public class PolicyRepo : IPolicyRepo
+    {
+        private IConfiguration _configuration;
+        private IMongoCollection<Policy> _MongoCollection;
+
+        public PolicyRepo(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var mongoClient = new MongoClient(_configuration["ConnectionString"]);
+
+            var database = mongoClient.GetDatabase(_configuration["DatabaseName"]);
+
+            _MongoCollection = database.GetCollection<Policy>(
+             _configuration["PoliciesCollectionName"]);
+
+        }
+        public Task<Policy> AddPolicy(Policy policy)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
